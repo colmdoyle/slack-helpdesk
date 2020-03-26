9083b80a05e50cc88b9334c6e4641d6b5c0b7002
Colm Doyle <cdoyle@slack-corp.com>
2020-03-26 10:47:02 +0000

    comments & docs

--- src/app.ts
+++ src/app.ts
@@ -58,6 +58,8 @@ app.view({ callback_id: modalCallbacks.createIssue }, ({ ack, body, context }) =
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const priorityID = state.values[modalFields.urgency][modalFields.urgency].selected_option!.value;
 
+    
+    // Create JIRA Issue
     axios.post(`${atlassianAPIURL}/issue`, {
         fields: {
             summary,
@@ -81,16 +83,17 @@ app.view({ callback_id: modalCallbacks.createIssue }, ({ ack, body, context }) =
         }
     }
     ).then((response) => {
+        // Update modal with issue details
         ack({
             response_action: 'update',
             view: issueCreatedModal(response.data.key)
         });
+        // DM Ticket details to the user
         app.client.chat.postMessage({
             token: context.botToken,
             text: `Thanks for getting in touch about "*${summary}*". We're tracking your request in ticket *<${process.env.ATLASSIAN_BASE_WEB_UI}/projects/HELP/issues/${response.data.key}|${response.data.key}>*. Have a great day!`,
             channel: body.user.id
         });
-        console.log(response.data);
     }).catch((error) => {
         console.error(error);
     })
